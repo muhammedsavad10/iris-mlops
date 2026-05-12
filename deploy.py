b8d549035bdbaca747a2dd99e25485827d0c4a5d
Muhammed Savad T M <muhammedsavadmammu@gmail.com>
2026-05-12 20:58:29 +0530

    Initial commit: Building the CI/CD Pipeline

---
+++ deploy.py
@@ -0,0 +1,48 @@
+import sagemaker
+from sagemaker.sklearn.model import SKLearnModel
+import time
+import boto3
+
+role="arn:aws:iam::285407030131:role/sagemaker_execution_role"
+
+boto_session = boto3.Session(region_name="eu-north-1")
+session = sagemaker.Session(boto_session=boto_session)
+
+
+endpoint_name = f"iris-endpoint--{int(time.time())}"
+
+model = SKLearnModel(
+    model_data="s3://muhammed-sagemaker-models-123/model.tar.gz",
+    role=role,
+    entry_point="inference.py",  
+    framework_version="1.2-1",
+    sagemaker_session=session
+)
+
+# 🔍 Check if endpoint exists
+sm_client = boto3.client("sagemaker", region_name="eu-north-1")
+
+try:
+    sm_client.describe_endpoint(EndpointName=endpoint_name)
+    endpoint_exists = True
+except:
+    endpoint_exists = False
+
+# 🚀 Deploy logic
+if endpoint_exists:
+    print("🔄 Updating existing endpoint...")
+    predictor = model.deploy(
+        instance_type="ml.t2.medium",
+        initial_instance_count=1,
+        endpoint_name=endpoint_name,
+        update_endpoint=True
+    )
+else:
+    print("🚀 Creating new endpoint...")
+    predictor = model.deploy(
+        instance_type="ml.t2.medium",
+        initial_instance_count=1,
+        endpoint_name=endpoint_name
+    )
+
+print("✅ Deployment complete")
